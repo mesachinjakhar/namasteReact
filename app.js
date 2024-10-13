6b9cac8c5e33592be588ec7d2999cb4e2fcdf28d
sachin jakhar <mesachinjakhar@gmail.com>
2024-10-13 23:18:52 +0530

    day 2 - npm & parcel

---
+++ app.js
@@ -0,0 +1,27 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+
+const reactHeading = React.createElement("h1", {
+    id: "heading",
+    style: {
+        color: "red"
+    }
+}, "Hello World from React")
+
+
+console.log(reactHeading);
+
+const reactRoot = ReactDOM.createRoot(document.getElementById("root"))
+
+reactRoot.render(reactHeading)
+
+
+const reactDiv = React.createElement("div", {
+    id: "parent"
+}, React.createElement("div", {
+    id: "child"
+}, [React.createElement("h1", {}, "Hello from h1 tag"),  React.createElement("h2", {}, "hello from h2 tag")]))
+
+
+reactRoot.render(reactDiv)
